bebdddbf82a3b5dc0519b598bc6457ae2293793e
Vitalii <vittiger09@gmail.com>
2020-07-27 10:04:03 +0300

    first commit

---
+++ my-site/unit-project/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        nav: true,
+        navText: ['', ''],
+        loop: true,
+        smartSpeed: 1000
+    });
+  });
